f467ddf4e01e71470196b9cbf0d5fbe41abec839
canyalcin278 <70910006+canyalcin278@users.noreply.github.com>
2020-09-14 15:21:13 +0300

    Create Starting php #13switch.php

---
+++ Starting php #13switch.php
@@ -0,0 +1,23 @@
+<?php
+/* The switch statement is used to perform different actions based on different conditions.
+Use the switch statement to select one of many blocks of code to be executed.
+*/
+
+
+$favcolor = "red";
+
+switch ($favcolor) {
+  case "mercedes":
+    echo "mercedes";
+    break;
+  case "bmw":
+    echo "bmw";
+    break;
+  case "lamborgini":
+    echo "lamborgini";
+    break;
+  default:
+   
+}
+/* this is how you use swtitch*/
+?>
