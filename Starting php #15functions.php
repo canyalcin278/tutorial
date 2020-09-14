7265ebd73c6b6ab602cf59ea114a9baedd8ebd5a
canyalcin278 <70910006+canyalcin278@users.noreply.github.com>
2020-09-14 16:40:05 +0300

    Create Starting php #15functions.php

---
+++ Starting php #15functions.php
@@ -0,0 +1,18 @@
+<?php 
+/* HOW TO CREAT YOUR OWN FUNCTİON */
+
+/*it should be like this*/
+
+
+function functionName() {
+  code ;
+}
+/*example*/
+
+function Msg() {
+  echo "Hello!";
+}
+
+Msg();
+
+?>
