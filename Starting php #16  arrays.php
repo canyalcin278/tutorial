041fda2fbbc08c7daab961c568980ad5d536c1c0
canyalcin278 <70910006+canyalcin278@users.noreply.github.com>
2020-09-14 16:42:36 +0300

    Create Starting php #16  arrays.php

---
+++ Starting php #16  arrays.php
@@ -0,0 +1,14 @@
+<?php
+/*An array stores multiple values*/
+
+example 
+
+/*
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+*/
+
+
+
+
+?>
